a0e698c3cf0e31fef5a6a411e3f635e4eca6a1d8
saleira <goncalo.caro@gmail.com>
2024-10-02 18:05:34 +0100

    Cheking directories paths

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-
-# importing os module
 import os
- 
-# Specify path
-path = '~/sync2Folders'
- 
-# Check whether the specified
-# path exists or not
-isExist = os.path.exists(path)
-print(isExist)
+import argparse
+
+path = '.\source'
+
+def sync_folders(source, replica):
+  if os.path.isdir(source):
+    print(f"{source} exists")
+  else:
+    print(f"{source} does not exists")
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Synchronize two folders periodically.")
+  parser.add_argument("source", help="Path to the source folder")
+  parser.add_argument("replica", help="Path to the replica folder")
+  #parser.add_argument("interval", type=int, help="Synchronization interval in seconds")
+  #parser.add_argument("log_file", help="Path to the log file")
+
+  args = parser.parse_args()
+  sync_folders(args.source, args.replica)
  
  
