a9b79e3a13a82fa1e70411cf888204cf384f8cb0
saleira <goncalo.caro@gmail.com>
2024-10-02 11:13:59 +0100

    test

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+# importing os module
+import os
+ 
+# Specify path
+path = '~/sync2Folders'
+ 
+# Check whether the specified
+# path exists or not
+isExist = os.path.exists(path)
+print(isExist)
+ 
+ 
